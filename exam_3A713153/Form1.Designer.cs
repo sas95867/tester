8f20e0839dac81394cd5ce44b2a71faf7ff3ae86
喆斌 蔡 <sas95867@gmail.com>
2020-11-12 22:53:04 +0800

    加入專案檔案。

---
+++ exam_3A713153/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace exam_3A713153
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbInput = new System.Windows.Forms.TextBox();
+            this.btnGo = new System.Windows.Forms.Button();
+            this.lab1 = new System.Windows.Forms.Label();
+            this.pic2 = new System.Windows.Forms.PictureBox();
+            this.pic1 = new System.Windows.Forms.PictureBox();
+            this.rtbout = new System.Windows.Forms.RichTextBox();
+            this.lab2 = new System.Windows.Forms.Label();
+            this.btn2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pic2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tbInput
+            // 
+            this.tbInput.Location = new System.Drawing.Point(443, 99);
+            this.tbInput.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.tbInput.Name = "tbInput";
+            this.tbInput.Size = new System.Drawing.Size(342, 27);
+            this.tbInput.TabIndex = 0;
+            // 
+            // btnGo
+            // 
+            this.btnGo.BackColor = System.Drawing.Color.Yellow;
+            this.btnGo.Font = new System.Drawing.Font("微軟正黑體", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnGo.ForeColor = System.Drawing.Color.DarkOrchid;
+            this.btnGo.Location = new System.Drawing.Point(204, 37);
+            this.btnGo.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(109, 151);
+            this.btnGo.TabIndex = 3;
+            this.btnGo.Text = "點我確定數字";
+            this.btnGo.UseVisualStyleBackColor = false;
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // lab1
+            // 
+            this.lab1.AutoSize = true;
+            this.lab1.Font = new System.Drawing.Font("微軟正黑體", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.lab1.Location = new System.Drawing.Point(448, 64);
+            this.lab1.Name = "lab1";
+            this.lab1.Size = new System.Drawing.Size(134, 31);
+            this.lab1.TabIndex = 4;
+            this.lab1.Text = "輸入數字吧";
+            // 
+            // pic2
+            // 
+            this.pic2.Image = global::exam_3A713153.Properties.Resources._009;
+            this.pic2.Location = new System.Drawing.Point(52, 250);
+            this.pic2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.pic2.Name = "pic2";
+            this.pic2.Size = new System.Drawing.Size(261, 342);
+            this.pic2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic2.TabIndex = 2;
+            this.pic2.TabStop = false;
+            // 
+            // pic1
+            // 
+            this.pic1.Location = new System.Drawing.Point(443, 250);
+            this.pic1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.pic1.Name = "pic1";
+            this.pic1.Size = new System.Drawing.Size(338, 342);
+            this.pic1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic1.TabIndex = 1;
+            this.pic1.TabStop = false;
+            // 
+            // rtbout
+            // 
+            this.rtbout.Location = new System.Drawing.Point(443, 159);
+            this.rtbout.Name = "rtbout";
+            this.rtbout.Size = new System.Drawing.Size(246, 29);
+            this.rtbout.TabIndex = 5;
+            this.rtbout.Text = "";
+            this.rtbout.TextChanged += new System.EventHandler(this.rtbout_TextChanged);
+            // 
+            // lab2
+            // 
+            this.lab2.AutoSize = true;
+            this.lab2.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.lab2.Location = new System.Drawing.Point(452, 135);
+            this.lab2.Name = "lab2";
+            this.lab2.Size = new System.Drawing.Size(92, 25);
+            this.lab2.TabIndex = 6;
+            this.lab2.Text = "猜的次數";
+            // 
+            // btn2
+            // 
+            this.btn2.Font = new System.Drawing.Font("微軟正黑體", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btn2.Location = new System.Drawing.Point(52, 37);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(103, 150);
+            this.btn2.TabIndex = 7;
+            this.btn2.Text = "點我重置要猜的數字";
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Silver;
+            this.ClientSize = new System.Drawing.Size(900, 683);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.lab2);
+            this.Controls.Add(this.rtbout);
+            this.Controls.Add(this.lab1);
+            this.Controls.Add(this.btnGo);
+            this.Controls.Add(this.pic2);
+            this.Controls.Add(this.pic1);
+            this.Controls.Add(this.tbInput);
+            this.Font = new System.Drawing.Font("微軟正黑體", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "猜猜數字並獲得與下方相同的圖片";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pic2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbInput;
+        private System.Windows.Forms.PictureBox pic1;
+        private System.Windows.Forms.PictureBox pic2;
+        private System.Windows.Forms.Button btnGo;
+        private System.Windows.Forms.Label lab1;
+        private System.Windows.Forms.RichTextBox rtbout;
+        private System.Windows.Forms.Label lab2;
+        private System.Windows.Forms.Button btn2;
+    }
+}
+
---
+++ exam_3A713153/Form1.cs
@@ -0,0 +1,108 @@
+﻿using exam_3A713153.Properties;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace exam_3A713153
+{
+    public partial class Form1 : Form
+    {
+        List<Image> list = new List<Image>();
+        int x = (new Random()).Next(100);
+       
+
+        int a;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            MessageBox.Show("比大小換圖片", "開始囉!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            list.Add(Resources._002);
+            list.Add(Resources._003);
+            list.Add(Resources._004);
+            list.Add(Resources._005);
+            list.Add(Resources._006);
+            list.Add(Resources._007);
+            list.Add(Resources._008);
+            list.Add(Resources._009);
+        }
+
+        private void btnGo_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                //提取輸入內容
+                string input_str = tbInput.Text;
+                //轉換int 
+                int input_int = int.Parse(input_str);
+                
+                //TODO: 寫判斷邏輯
+                if (input_int < x)
+                {
+                    int index = (new Random()).Next(7);//模擬隨機產生 一個值;
+                    
+                    pic1.Image = list[index];
+                    MessageBox.Show($"{input_int}~?? 還要再大點", "NoNoNo", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+                    a = a + 1;
+                }
+                else if (input_int > x)
+                {
+                    int index = (new Random()).Next(3);//模擬隨機產生 一個值;
+
+                    pic1.Image = list[index];
+                    MessageBox.Show($"??~{input_int} 在小一點", "NoNoNo", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+                    a = a + 1;
+                }
+                else if (input_int == x)
+                {
+                    int index = 7;
+
+                    pic1.Image = list[index];
+                    //顯示內容
+                    MessageBox.Show($"沒錯!就是{input_int}", "YesYesYes", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    a = a + 1;
+                }
+                rtbout.Text=$"你猜的次數為{a}次!";
+            }
+            catch (Exception)
+            {
+                //錯誤提示
+                int index = 3 ;
+
+                pic1.Image = list[index];
+                MessageBox.Show($"怎麼連數字都不想打阿", "我問號", MessageBoxButtons.OK, MessageBoxIcon.Question);
+                //throw;
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            MessageBox.Show("關起來","爛遊戲別玩了",MessageBoxButtons.OK,MessageBoxIcon.Error);
+        }
+
+        private void rtbout_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            x = (new Random()).Next(100);
+            a = 0;
+            rtbout.Text = " ";
+
+        }
+    }
+}
